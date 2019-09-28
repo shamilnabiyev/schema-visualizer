960b14a56edfcb6028115a00d80feb1288766aa7
istshnabi <shamil.nabiyev@stud.h-da.de>
2019-09-28 22:57:00 +0200

    Change primary color to 'corporate blue'

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -10,7 +10,7 @@ var myPieChart = new Chart(ctx, {
     labels: ["Direct", "Referral", "Social"],
     datasets: [{
       data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      backgroundColor: ['#0081c9', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
