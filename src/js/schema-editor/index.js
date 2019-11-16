86106315bc5d09276473bebcbf6fab014a0768fb
istshnabi <shamil.nabiyev@stud.h-da.de>
2019-11-16 09:41:20 +0100

    Small fixes

--- src/js/schema-editor/index.js
+++ src/js/schema-editor/index.js
@@ -10,9 +10,10 @@ schemaEditorButton.on('click', function () {
     schemaEditorModal.modal('show');
 });
 
-schemaEditorModal.on('shown.bs.modal', function () {
+schemaEditorModal.on('show.bs.modal', function () {
     const row1 = createSeObjectRow('ROOT', 'object');
 
+    $('#schema-editor').empty();
     $('#schema-editor').append(row1);
     $('#schema-editor > .new-prop-container > .new-field-elements > .add-btn').prop("disabled", true);
     $('#schema-editor > .object-row > .form-inline > .remove-btn-block > .remove-btn').first().remove();
